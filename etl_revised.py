020f9f766c89bbcb0389c76affbff517e71476bb
Quasi1412 <premkaushyk1412@gmail.com>
2025-09-15 17:42:43 -0400

    Added print statement for debug and implemented normalisation

--- etl_revised.py
+++ etl_revised.py
@@ -2,8 +2,10 @@ import pandas as pd
 import os
 from bedrock_pdf_to_csv import extract_pdf_with_bedrock
 from process_statements import statement_to_df
+from normalise_data import normalise_df
 
 parent_directory = "./statements"
+ 
 all_data = []
 
 
@@ -14,20 +16,16 @@ for card_folder in os.listdir(parent_directory):
 
     for data_file in os.listdir(folder_path):
         file_path = os.path.join(folder_path,data_file)
+        print(f"Processing {file_path}")
         df = statement_to_df(file_path)
-        if not df.empty:
+        if df is not None and not df.empty:
             all_data.append(df)
     
     if all_data:
         card_df = pd.concat(all_data,ignore_index=True)
         
+        card_df = normalise_df(card_df)
         card_df['card'] = card_folder
-        
-        
-            
-        
-        
-    
         
 
  
