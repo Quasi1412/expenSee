118605e7727d002da7b624a74ab553480c9d17c6
Quasi1412 <premkaushyk1412@gmail.com>
2025-09-16 22:29:25 -0400

    Added logic to normalise different date formats

--- normalise_data.py
+++ normalise_data.py
@@ -1,6 +1,15 @@
 import pandas as pd
 from io import StringIO
 
+def normalise_date(d):
+    
+    default_year = "2025" #Need to be fixed
+    
+    try: 
+        return pd.to_datetime(d,errors='coerce')
+    except Exception:
+        return pd.to_datetime(f"{d} {default_year}")
+
 def normalise_df(df):
     
     df.columns = [col.strip().lower().replace(' ','_') for col in df.columns]
@@ -8,14 +17,13 @@ def normalise_df(df):
     date_col = next((col for col in df.columns if 'date' in col),None)
     description_col = next((col for col in df.columns if 'description' in col or 'details' in col))
     amount_col = next((col for col in df.columns if 'amount' in col or 'charges' in col or 'debit' in col))
+    category_col = next((col for col in df.columns if 'category' in col),None)
     
-    #Assuming default year, need to fix in future
-    year_default = 2025
+    filtered_df = df[[date_col,description_col,category_col,amount_col]]
+    filtered_df.columns = ['transaction_date','description','category','amount']
     
-    filtered_df = df[[date_col,description_col,amount_col]]
-    filtered_df.columns = ['transaction_date','description','amount']
-    
-    filtered_df['transaction_date'] = filtered_df['transaction_date'].apply(lambda d: pd.to_datetime(f"{d}/{year_default}",format="%m/%d/%Y"))
+
+    filtered_df['transaction_date'] = filtered_df['transaction_date'].apply(normalise_date)
     filtered_df['amount'] = pd.to_numeric(filtered_df['amount'].astype(str).str.replace(r"[^\d.-]","",regex=True),errors='coerce')
     
     return(filtered_df)
