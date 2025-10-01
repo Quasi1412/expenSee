48c8b4bd055e2a021051c7a10aaa268f1901d611
Quasi1412 <premkaushyk1412@gmail.com>
2025-09-30 23:39:03 -0400

    updated as per orchestration flow

--- load/add_to_postgresdb.py
+++ load/add_to_postgresdb.py
@@ -4,15 +4,16 @@ import os
 
 
 
-def push_data_to_db(transaction_df, PG_URL):
+def push_data_to_db(csv_file, PG_URL):
     
+    transaction_df = pd.read_csv(csv_file)
     engine = create_engine(PG_URL)
     
     #Get schema from df
     transaction_df.head(0).to_sql("expenses", con=engine, if_exists='replace', index=False)
     print("Table has been created!!")
     
-    print(f"Inserting {transaction_df['card_name'].unique()} data into {PG_DATABASE}")
+    print(f"Inserting {transaction_df['card_name'].unique()} data into Database")
     transaction_df.to_sql("expenses",con=engine, if_exists='append', index=False)
     
     print(f"{transaction_df['card_name'].unique()} has been inserted ✅") 
