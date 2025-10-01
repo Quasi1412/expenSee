0b2364bc55d67fcf50c0c34d874590bc43e52128
Quasi1412 <premkaushyk1412@gmail.com>
2025-09-30 23:37:57 -0400

    Added load_data_task

--- airflow/dags/etl_dag.py
+++ airflow/dags/etl_dag.py
@@ -20,7 +20,7 @@ PG_PASSWORD = os.getenv('PG_PASSWORD')
 PG_HOST = os.getenv('PG_HOST')
 PG_PORT = os.getenv('PG_PORT')
 PG_DATABASE = os.getenv('PG_DATABASE')
-PG_URL = f'postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DATABASE}'
+PG_CONN_URL = f'postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DATABASE}'
 
 STATEMENTS_DIR = os.getenv('STATEMENTS_DIR','/opt/airflow/statements')
 FINAL_DF_DIR = '/opt/airflow/final_df.csv'
@@ -39,4 +39,10 @@ with local_workflow:
         op_kwargs = dict(input_files = "{{ti.xcom_pull(task_ids = 'extract_data_from_statements')}}",output_file = FINAL_DF_DIR),
     )
     
-fetch_data_task >> transform_data_task
+    load_data_task = PythonOperator(
+        task_id = 'load_data_to_postgres',
+        python_callable = push_data_to_db,
+        op_kwargs = dict(csv_file = "{{ti.xcom_pull(task_ids = 'transform_by_normalising_data')}}",PG_URL = PG_CONN_URL)
+    )
+    
+fetch_data_task >> transform_data_task >> load_data_task
